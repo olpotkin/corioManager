9db0fc91a235c83329b47de7ce293712da795926
olpotkin <olpotkin@gmail.com>
2016-09-20 16:29:07 -0700

    Add template for window's x,y-coordinates request

--- Management.py
+++ Management.py
@@ -16,4 +16,20 @@ class corioManager:
         corioClient.send_command("Preset.Take = 1")
         corioClient.send_command("Layout1.StbdActive")
 
+    # TODO: Get current position of the Window
+    # Request: "Window<N>", where N - number of window
+    # for example, Window3
 
+    # TODO: Move Window inside Canvas: X and Y axes (required existing Canvas in Preset)
+
+    # HELPERS
+
+    def get_window_physical_center_x(self, corioClient):
+        center_x = corioClient.send_command("Window3")
+
+        return center_x
+
+    def get_window_physical_center_y(self, corioClient):
+        center_y = corioClient.send_command("Window3")
+
+        return center_y
