42efce77d2449f9a22855108f3eea2955651c82c
olpotkin <olpotkin@gmail.com>
2016-09-20 14:39:12 -0700

    Console input to call method 'Switch preset'

--- app.py
+++ app.py
@@ -12,6 +12,24 @@ logging.basicConfig(level=logging.INFO)
 # IP address of corioMaster Unit
 corioClient = Client('127.0.0.1')
 
+# Get an instance of the class
+corioManager = Management.corioManager()
 
-corioManager = Management.corioManager()    # get an instance of the class
-corioManager.change_preset(corioClient)     # call the method of the instance
+
+# Console input
+user_cmd = None
+while not user_cmd:
+    user_cmd = raw_input()
+    if user_cmd == 'preset':
+        # Switch preset
+        corioManager.change_preset(corioClient)
+    else:
+        print ('Command not found, try again...\n')
+
+
+
+
+
+
+
+
