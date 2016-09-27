a129dd196c80dcc22871c8eced2fb1dec92506a2
olpotkin <olpotkin@gmail.com>
2016-09-27 10:28:49 -0700

    Fixed error with names of parameters

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
 # IP address of corioMaster Unit
-corioClient = Client('169.254.6.16')
+#corioClient = Client('169.254.6.16')
+corioClient = Client('172.24.103.148')
 
 # Get an instance of the class
 corioManager = Management.corioManager()
@@ -28,7 +29,7 @@ def hello():
     return "corioManager!"
 
 # 2. Set preset by id
-@app.route("/set_preset/<int:id>")
+@app.route("/setpreset/<int:preset_id>")
 def set_preset(preset_id):
     try:
         corioManager.change_preset(corioClient, preset_id)
@@ -36,18 +37,17 @@ def set_preset(preset_id):
     except:
         return "Unit is not connected!"
 
+
 # 3. Move window
 # TODO: 3.1. Move right (window_id, +X, 0)
 # TODO: 3.2. Move left  (window_id, -X, 0)
 # TODO: 3.3. Move up    (window_id, 0, +Y)
 # TODO: 3.4. Move down  (window_id, 0, -Y)
-@app.route("/move_window/<int:id>/<int:moveX>/<int:moveY>")
+@app.route("/move_window/<int:window_id>/<int:moveX>/<int:moveY>")
 def move_window(window_id, moveX, moveY):
     corioManager.move_window(window_id, moveX, moveY)
     # TEST RETURNING DATA!!!
     data = corioManager.get_window_physical_center_x(corioClient)
-
-
 
 
 if __name__ == "__main__":
